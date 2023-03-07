629dad60d286033a3b4c36a31c72b0140d1c7084
Alan Coiradas <coiradasalan@gmail.com>
2023-03-07 20:58:27 -0300

    Se hicieron ajustes en el código js de las páginas past.js y main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 const cards = document.getElementById("cards_box");
 let tarjetas = '';
 for (i = 0; i < data.events.length; i++) {
-    tarjetas += `<div class="card" style="width: 24rem;">
-                    <img src="${data.events[i].image}" class="card-img-top" alt="event_image" height="200px">
-                    <div class="card-body">
+    tarjetas += `<div class="card m-2" style="width: 24rem;">
+                    <div class="image_container">
+                        <img src="${data.events[i].image}" class="card-img-top" alt="event_image">
+                        </div>
+                        <div class="card-body">
                         <h5 class="card-title">${data.events[i].name}</h5>
-                        <p class="card-text mt-1">${data.events[i].date}</p>
-                        <p class="card-text text-center"><i>${data.events[i].description}</i></p>
+                        <p class="card-text my-2">${data.events[i].date}</p>
+                        <p class="card-text text-center card-description"><i>${data.events[i].description}</i></p>
                         <p class="card-text"><b>Category:</b> ${data.events[i].category}</p>
                         <p class="card-text"><b>Place:</b> ${data.events[i].place}</p>
                         <p class="card-text"><b>Capacity:</b> ${data.events[i].capacity}</p>
                         <p class="card-text"><b>Assistance:</b> ${data.events[i].assistance}</p>
                         <p class="card-text"><b>Price:</b> $${data.events[i].price}</p>
-                        <a href="./html/details.html" class="btn btn-primary">More Details</a>
+                        <a href="./details.html" class="btn btn-primary">More Details</a>
                     </div>
                 </div>`
 }
